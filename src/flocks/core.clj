3c5602590ecd6a84a5a37401e70ed1bd15c94ff3
Martin Maugeais <mmaugeais@watermarkinsights.com>
2021-09-16 21:33:14 +0200

    pull stuff out

--- src/flocks/core.clj
+++ src/flocks/core.clj
@@ -2,42 +2,24 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(def flocksize 10)
 (load "utils")
+(load "flock")
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
-  (def flocksize 10)
-  (defn randcoords [] ([(rand-int (q/width)) (rand-int (q/height))]))
-  (def initstate (vec (map (comp ref vec) (repeatedly flocksize randcoords))))
   ; setup function returns initial state. 
-  {:flock initstate})
+  {:flock (initstate q/width q/height)})
 
 (defn update-state [state]
   {})
 
-(defn draw-bird [pos]
-  (q/triangle 10 10 20 30 30 20)
-  ; (let [angle 45
-  ;       x (* 150 (q/cos angle))
-  ;       y (* 150 (q/sin angle))
-  ;       pos [20 20]]
-  ;   ; Move origin point to the center of the sketch.
-  ;   (q/with-translation [(/ (q/width) 2)
-  ;                        (/ (q/height) 2)]
-; ))
-)
-
-(defn draw-flock [flocksize]
-      (draw-bird [10 20]))
-
-
 (defn draw-state [state]
   (q/background 240)
   (q/fill 0)
-  ; Calculate x and y coordinates of the circle.
   (draw-flock 4)
 )
 
@@ -56,4 +38,3 @@
   :middleware [m/fun-mode])
 
 (defn -main [& args])
-
