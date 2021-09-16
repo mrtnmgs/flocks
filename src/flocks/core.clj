72d9e9e54ecf83e9339f6a66d0dd99ba40e4a0bf
Martin Maugeais <mmaugeais@watermarkinsights.com>
2021-09-16 22:56:37 +0200

    init state ok

--- src/flocks/core.clj
+++ src/flocks/core.clj
@@ -2,17 +2,22 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(def flocksize 10)
 (load "utils")
 (load "flock")
 
 (defn setup []
+  (def flocksize 10)
+  (def w (q/width))
+  (def h (q/height))
+
+  (flocksetup w h flocksize) ; defines initstate. Can't be done before we get w and h 
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
   ; setup function returns initial state. 
-  {:flock (initstate q/width q/height)})
+  ; (println (map deref initstate))
+  {:flock initstate})
 
 (defn update-state [state]
   {})
@@ -20,7 +25,7 @@
 (defn draw-state [state]
   (q/background 240)
   (q/fill 0)
-  (draw-flock 4)
+  (draw-flock state)
 )
 
 (q/defsketch flocks
--- src/flocks/flock.clj
+++ src/flocks/flock.clj
@@ -1,8 +1,10 @@
-(defn randcoords [w h] ([(rand-int w) (rand-int h)]))
-(defn initstate [w h] (vec (map (comp ref vec) (repeatedly flocksize (randcoords w h)))))
+(defn flocksetup [w h flocksize]
+  (defn randcoords [] [(rand-int w) (rand-int h)])
+  (def initstate (vec (map (comp ref vec) (repeatedly flocksize randcoords))))
+  )
 
-(defn draw-flock [flocksize]
-    ;(draw-bird [10 20])
-   (q/triangle 10 10 30 10 10 30)
+(defn draw-flock [flock]
+    ;(draw-bird [10 40])
+   (q/triangle 10 10 40 10 10 40)
 )
 
